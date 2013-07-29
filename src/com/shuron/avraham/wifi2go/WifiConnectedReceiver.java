e73d469acdb29855b15ae90eb30205eb5c5f1c60
Avraham Shukron <avraham.shukron@gmail.com>
2013-07-29 23:14:02 +0300

    Initial code upload

---
+++ src/com/shuron/avraham/wifi2go/WifiConnectedReceiver.java
@@ -0,0 +1,16 @@
+package com.shuron.avraham.wifi2go;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class WifiConnectedReceiver extends BroadcastReceiver {
+
+	private Context mContext;
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
